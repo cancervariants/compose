789de1c533e0455a704a4122e96bbfab5491a287
Brian Walsh <walsbr@ohsu.edu>
2021-04-09 08:00:19 -0700

    Corrects comment

--- test/tests/integration/test_gene.py
+++ test/tests/integration/test_gene.py
@@ -4,20 +4,20 @@ import requests
 
 
 def test_server_alive():
-    """Therapy server should return 200."""
+    """Gene server should return 200."""
     response = requests.get("http://gene/gene")
     assert response.status_code == 200, test_server_alive.__doc__
 
 
 def test_swagger_ui():
-    """Therapy server should return swagger UI 'FastAPI - Swagger UI'."""
+    """Gene server should return swagger UI 'FastAPI - Swagger UI'."""
     response = requests.get("http://gene/gene")
     assert response.status_code == 200, test_swagger_ui.__doc__
     assert 'FastAPI - Swagger UI' in response.text, test_swagger_ui.__doc__
 
 
 def test_query():
-    """Therapy server should find `BRCA2`."""
+    """Gene server should find `BRCA2`."""
     url = "http://gene/gene/search?q=BRCA2&keyed=true"
     headers = {'accept': 'application/json'}
     response = requests.get(url, headers=headers)
